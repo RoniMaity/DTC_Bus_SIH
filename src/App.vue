092817fead5e976d190ffa6a6662284b5fb8b1c9
Arohi-jd <arohijadhav172@gmail.com>
2024-12-02 18:24:16 +0530

    cleanup and set up new project

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+   <!-- <img alt ="dtbusc" src="./assets/delhi_transport_corporation_web_banner_1.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+   <!-- <delhi_transport_corporation_web_banner_1/> -->
+  <HomePage></HomePage>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue';
+// import delhi_transport_corporation_web_banner_1 from './assets/delhi_transport_corporation_web_banner_1.png';
+
+export default {
+  name: 'App',
+  components: {
+  HomePage,
+  // delhi_transport_corporation_web_banner_1,
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
